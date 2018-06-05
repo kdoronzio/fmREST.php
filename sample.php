918dad7fb65da08ee1d95d1d3a5cd05e176f9dd4
kdoronzio <ken@paradisepartners.com>
2018-06-05 10:32:01 -0400

    Add files via upload

--- sample.php
+++ sample.php
@@ -1,6 +1,6 @@
 <?PHP
 
-error_reporting(E_ALL);
+error_reporting(1);
 ini_set('display_errors', true );
 
 if (!isset ($_REQUEST['action'])) $_REQUEST['action']='';
@@ -10,21 +10,31 @@ include_once ('fmREST.php');
 
  $debug=1;
 
-$host = 'localhost';
-$db = 'Contacts';
-$layout = 'Contacts';
+$host = 'fm17-inhouse.paradisepartners.com';
+$db = 'test';
+$layout = 'data';
 $user = 'admin';
 $pass = 'paradise';
 
-$fm = new fmREST ($host, $db, $layout, $user, $pass);
+$fm = new fmREST ($host, $db, $user, $pass, $layout);
 
 
 if ($_REQUEST['action'] == 'createrecord') {
 	//create record
 	$record['First'] = $_REQUEST['first'];
 	$record['Last'] =  $_REQUEST['last'];
-	$data['data'] =  $record;
+	$data['fieldData'] =  $record;
+	$data['script'] = 'duplicate record';
+	$data['script.param'] = 'new parameter';
 	$result = $fm -> createRecord ($data);
+}
+
+elseif ($_REQUEST['action'] == 'deleterecord') {
+	//delete record
+	$recordId = $_REQUEST['recordid'];
+	$data['script'] = 'duplicate record';
+	$data['script.param'] = 'new parameter';
+	$result = $fm -> deleteRecord ($recordId, $data); 
 }
 
 elseif ($_REQUEST['action'] == 'editrecord') {
@@ -33,7 +43,7 @@ elseif ($_REQUEST['action'] == 'editrecord') {
 	$record['First'] = $_REQUEST['first'];
 	$record['Last'] =  $_REQUEST['last'];
 
-	$data['data'] =  $record;
+	$data['fieldData'] =  $record;
 
 	$result = $fm -> editRecord ($recordId, $data); 
 } 
@@ -41,37 +51,56 @@ elseif ($_REQUEST['action'] == 'editrecord') {
 elseif ($_REQUEST['action'] == 'getrecord') {
 	//get record
 	$recordId = $_REQUEST['recordid'];
-	$result = $fm -> getRecord ($recordId); 
+	$parameters['script'] = 'duplicate record';
+	$parameters['script.param'] = 'new parameter';
+	$result = $fm -> getRecord ($recordId, $parameters); 
 }
 
 elseif ($_REQUEST['action'] == 'getrecords') {
 	//get records
-	$parameters = "offset=1&range=50";
+//	$parameters['_limit'] = 1;
+//	$parameters['script'] = 'duplicate record';
+//	$parameters['script.param'] = 'new parameter';
 	$result = $fm -> getRecords ($parameters); 
 
+}
+
+elseif ($_REQUEST['action'] == 'uploadcontainer') {
+	//upload container
+	$recordId = $_REQUEST['recordid'];
+	$fieldName = 'container';
+	$file = $_FILES['file'];
+	$result = $fm -> uploadContainer ($recordId, $fieldName, $file ); 
 }
 
 elseif ($_REQUEST['action'] == 'findrecords') {
 	//find records
 	$request1['first'] = $_REQUEST['first'];
 	$query = array ($request1);
-	$criteria['query'] = $query;
-	$result = $fm -> findRecords ($criteria); 
+	$data['query'] = $query;
+//	$data['limit'] = 2;
+//	$data['script'] = 'find script';
+//	$data['script.param'] = 'new parameter';
+	$result = $fm -> findRecords ($data); 
 }
 
-elseif ($_REQUEST['action'] == 'deleterecord') {
-	//delete record
-	$recordId = $_REQUEST['recordid'];
-	$result = $fm -> deleteRecord ($recordId); 
+elseif ($_REQUEST['action'] == 'setglobalfields') {
+	//set global field(s)
+	$fields['TEST::global'] = $_REQUEST['global'];
+	$data['globalFields'] = $fields;
+	$result = $fm -> setGlobalFields ($data); 
 }
+
+
+
 
 elseif ($_REQUEST['action'] == 'login') {
-	//delete record
+	//login
 	$result = $fm -> login (); 
 }
 
 elseif ($_REQUEST['action'] == 'logout') {
-	//delete record
+	//logout
 	$result = $fm -> logout (); 
 }
 
@@ -87,26 +116,30 @@ elseif ($_REQUEST['action'] == 'logout') {
 <h3>fmREST.php</h3>
 <h4>soSIMPLEsoftware.com</h4>
 	<div style="margin: 50px;">
-		<form method='post'>
+		<form method='post' enctype="multipart/form-data">
 		
 			<!--User <input name='user'></input><br />
 			Password <input name='password'></input><br /-->
 			Record ID <input name='recordid'></input><br />
 			First <input name='first'></input><br />
 			Last <input name='last'></input><br />
+			Global <input name='global'></input><br />
+			File <input name='file' type='file'></input><br />
 			<br />
 
 			  <!--input type="radio" name="action" value="login" > Login<br>
 			  <input type="radio" name="action" value="logout" > Logout<br-->
-			  <input type="radio" name="action" value="createrecord" > Create Record<br>
-			  <input type="radio" name="action" value="deleterecord" > Delete Record<br>
-			  <input type="radio" name="action" value="getrecord" > Get Record<br>
+			  <input type="radio" name="action" value="createrecord" > Create Record <small>(uses First & Last)</small><br>
+			  <input type="radio" name="action" value="deleterecord" > Delete Record <small>(uses Record ID)</small><br>
+			  <input type="radio" name="action" value="editrecord" > Edit Record <small>(uses Record ID & First & Last)</small><br>
+			  <input type="radio" name="action" value="getrecord" > Get Record <small>(uses Record ID)</small><br>
 			  <input type="radio" name="action" value="getrecords" > Get Records<br>
-			  <input type="radio" name="action" value="findrecords" > Find Records<br>
-			  <input type="radio" name="action" value="editrecord" > Edit Record<br><br>
-			  <input type="radio" name="action" value="login" > Log In Manually (happens automatically with all above actions)<br>
-			  <input type="radio" name="action" value="logout" > Log Out Manually (will automatically log out in 15 minutes)<br>
-			  <!--input type="radio" name="action" value="setglobal" > Set Global<br-->
+			  <input type="radio" name="action" value="uploadcontainer" > Upload Container <small>(uses Record ID & File)</small><br><br>
+			  <input type="radio" name="action" value="findrecords" > Find Records <small>(uses First)</small><br><br>
+			  <input type="radio" name="action" value="setglobalfields" > Set Global Fields <small>(uses Global)</small><br><br>
+			  			  
+			  <input type="radio" name="action" value="login" > Log In Manually <small>(happens automatically with all above actions)</small><br>
+			  <input type="radio" name="action" value="logout" > Log Out Manually <small>(will automatically log out in 15 minutes)</small><br>
 			<br/>
 			
 			<input type='submit' />
@@ -116,7 +149,17 @@ elseif ($_REQUEST['action'] == 'logout') {
 
 Request:
 <pre>
-	<?PHP print_r ($_REQUEST); ?>
+FILES: 
+<?PHP print_r ($_FILES); ?>
+
+POST: 
+<?PHP print_r ($_POST); ?>
+	
+GET: 
+<?PHP print_r ($_GET); ?>
+
+COOKIES: 
+<?PHP print_r ($_COOKIE); ?>
 </pre>
 
 Result:
